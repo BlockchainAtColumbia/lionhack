6d3eda2174263a66264fc7a6b520b66d0218badd
nturl <34818653+nturl@users.noreply.github.com>
2023-04-08 16:32:51 -0400

    Add files via upload

---
+++ js/custom_tvyLdFeZ.js
@@ -0,0 +1,16 @@
+/* Speakers Page (#1) */
+
+$(function(){
+  window.swiper102 = new Swiper('.swiper-102', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-102-next',
+      prevEl: '.swiper-102-prev',
+    },    
+    /*autoplay: {
+      delay: 5000,
+      disableOnInteraction: true, 
+    }*/
+  }); 
+});
